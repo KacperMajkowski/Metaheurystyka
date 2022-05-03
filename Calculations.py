a661fea25001578810ceae9bc2a2bbcd31b6c143
KacperMajkowski <kacper20015@o2.pl>
2022-05-03 15:45:15 +0200

    Calculating distance from permutation and distance matrix

--- Calculations.py
+++ Calculations.py
@@ -1,6 +1,13 @@
 
 def calculateDistance(permutation, distance_matrix):
     
-    # TODO: Calculate distance
-    #  return distance
-    pass
+    total_distance = 0
+    for i in range(len(permutation) - 1):
+        total_distance += distance_matrix[permutation[i]][permutation[(i + 1) % len(permutation)]]
+    return total_distance
+
+
+p = [2, 0, 1]
+dm = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+
+print(calculateDistance(p, dm))
